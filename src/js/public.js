ff2fba716861326050ef30c4b370f7f2604898eb
jl <601819456@qq.com>
2017-09-28 12:49:15 +0800

    鬼知道我在写什么
    鬼知道我在写什么

--- src/js/public.js
+++ src/js/public.js
@@ -1,5 +1,5 @@
 $(function(){
-	$('.main-lef').html('<div class=""  style="width: 180px;margin: 0 auto;">'+
+	$('.main-left').html('<div class=""  style="width: 180px;margin: 0 auto;">'+
     '<ul  class="navall">'+
     	 '<li><a class="actives">猜数字游戏</a></li>'+	    	
     	 '<li><a href="/">猜数字游戏</a></li>'+
